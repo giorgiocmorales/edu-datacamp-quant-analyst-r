523f9c686ebcde30dd2e75e474c3d07036ebee7d
Giorgio Cunto Morales <giorgio.cmorales@gmail.com>
2025-10-08 23:51:59 -0400

    Initialize standardized educational repo structure

---
+++ scripts/QA_01.R
@@ -0,0 +1,35 @@
+# Variables for starting_cash and 5% return during January
+starting_cash <- 200
+jan_ret <- 5
+jan_mult <- 1 + (jan_ret / 100)
+
+# How much money do you have at the end of January?
+post_jan_cash <- starting_cash*jan_mult
+
+# Print post_jan_cash
+post_jan_cash
+
+# January 10% return multiplier
+jan_ret_10 <- 10
+jan_mult_10 <- 1 + (jan_ret_10/100)
+
+# How much money do you have at the end of January now?
+post_jan_cash_10 <- starting_cash*jan_mult_10
+
+# Print post_jan_cash_10
+post_jan_cash_10
+
+# Starting cash and returns 
+starting_cash <- 200
+jan_ret <- 4
+feb_ret <- 5
+
+# Multipliers
+jan_mult <- 1.04
+feb_mult <- 1.05
+
+# Total cash at the end of the two months
+total_cash <- starting_cash*jan_mult*feb_mult
+
+# Print total_cash
+total_cash
